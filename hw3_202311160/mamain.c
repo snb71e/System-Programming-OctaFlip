80cbe40b409a2eb00409b69fbeda1b1eb066a6cb
snb71e <126742332+snb71e@users.noreply.github.com>
2025-06-01 15:08:47 +0900

    ..

---
+++ hw3_202311160/mamain.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "include/server.h"
+#include "include/client.h"
+#include "include/board.h"
+
+#define DEFAULT_PORT "8080"
+
+static void print_usage(const char *progname) {
+    fprintf(stderr,
+        "Usage:\n"
+        "  %s server [-p port]\n"
+        "  %s client -i ip -p port -u username\n",
+        progname, progname);
+}
+
+int main(int argc, char *argv[]) {
+
+    if (init_led_matrix(argc, argv) < 0) {
+        fprintf(stderr, "Failed to initialize LED Matrix. Exiting.\n");
+        return EXIT_FAILURE;
+    }
+
+    if (argc < 2) {
+        print_usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    if (strcmp(argv[1], "server") == 0) {
+        const char *port = DEFAULT_PORT;
+        int opt;
+        opterr = 0;
+        // parse server options
+        optind = 2;
+        while ((opt = getopt(argc, argv, "p:")) != -1) {
+            switch (opt) {
+                case 'p':
+                    port = optarg;
+                    break;
+                default:
+                    print_usage(argv[0]);
+                    return EXIT_FAILURE;
+            }
+        }
+        int ret = server_run(port);
+        close_led_matrix();
+        return ret;
+    }
+    else if (strcmp(argv[1], "client") == 0) {
+        char *ip = NULL;
+        char *port = NULL;
+        char *username = NULL;
+        int opt;
+        opterr = 0;
+        optind = 2;
+        while ((opt = getopt(argc, argv, "i:p:u:")) != -1) {
+            switch (opt) {
+                case 'i':
+                    ip = optarg;
+                    break;
+                case 'p':
+                    port = optarg;
+                    break;
+                case 'u':
+                    username = optarg;
+                    break;
+                default:
+                    print_usage(argv[0]);
+                    return EXIT_FAILURE;
+            }
+        }
+        if (!ip || !port || !username) {
+            print_usage(argv[0]);
+            return EXIT_FAILURE;
+        }
+        int ret = client_run(ip, port, username);
+        close_led_matrix();
+        return ret;
+    }
+    else {
+        print_usage(argv[0]);
+        close_led_matrix();
+        return EXIT_FAILURE;
+    }
+}
